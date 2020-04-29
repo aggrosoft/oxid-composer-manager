3ca8f05dce4f9e4ffdeb77cf8fd8d270d5ddf693
alexander <coding@aggrosoft.de>
2020-04-29 14:22:58 +0200

    bump version

--- copy_this/modules/composerman/metadata.php
+++ copy_this/modules/composerman/metadata.php
@@ -7,7 +7,7 @@ $aModule = array(
     'title'        => '<img src="' . oxRegistry::getConfig()->getShopUrl() . 'modules/composerman/out/img/logo-composer.png" height="15" alt="Composer Manager" title="Composer Manager"> Composer Manager',
     'description'  => 'Manage oxid eShop composer modules through admin interface',
     'thumbnail'    => '',
-    'version'      => '1.1.1',
+    'version'      => '1.1.2',
     'author'       => 'Aggrosoft GmbH',
     'extend'      => array(
 
